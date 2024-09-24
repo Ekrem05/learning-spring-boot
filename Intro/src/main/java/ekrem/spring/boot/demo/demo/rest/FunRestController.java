542aa023bb7054c8f0e64ab4ef45066b5eb05fbf
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-09-24 20:38:26 +0300

    Actuator, Secure endpoints, app config

--- Intro/src/main/java/ekrem/spring/boot/demo/demo/rest/FunRestController.java
+++ Intro/src/main/java/ekrem/spring/boot/demo/demo/rest/FunRestController.java
@@ -1,15 +1,18 @@
 package ekrem.spring.boot.demo.demo.rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    @Value("${greeting.name}")
+    private String username;
 
     @GetMapping("/")
     public String HelloWorld() {
-        return "Hello World";
+        return "Hello %s".formatted(username);
     }
 }
