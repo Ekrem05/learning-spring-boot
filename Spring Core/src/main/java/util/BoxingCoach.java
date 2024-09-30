801a520fdd5104ac4a72459d54ded4d041599023
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-09-30 09:53:17 +0300

    Lazy init

--- Spring Core/src/main/java/util/BoxingCoach.java
+++ Spring Core/src/main/java/util/BoxingCoach.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class BoxingCoach implements Coach {
+
+    public BoxingCoach() {
+        System.out.println("BoxingCoach initialized");
+    }
     @Override
     public String getDailyWorkout() {
         return "Shadow box for 15 minutes";
--- Spring Core/src/main/java/util/GymCoach.java
+++ Spring Core/src/main/java/util/GymCoach.java
@@ -1,10 +1,15 @@
 package util;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class GymCoach implements Coach {
 
+    public GymCoach() {
+        System.out.println("Gym Coach initialized");
+    }
     public String getDailyWorkout() {
         return "4 sets of bench press";
     }
