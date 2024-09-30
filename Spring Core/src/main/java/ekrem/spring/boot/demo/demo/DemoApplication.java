02912825c6781e694298fc52f2975363acefa90a
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-09-30 09:39:28 +0300

    Base package scanning

--- Spring Core/src/main/java/ekrem/spring/boot/demo/demo/DemoApplication.java
+++ Spring Core/src/main/java/ekrem/spring/boot/demo/demo/DemoApplication.java
@@ -3,9 +3,7 @@ package ekrem.spring.boot.demo.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-		scanBasePackages = {"java.ekrem.spring.boot.demo.demo",
-				"java.util"})
+@SpringBootApplication(scanBasePackages = {"ekrem.spring.boot.demo.demo","util"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
