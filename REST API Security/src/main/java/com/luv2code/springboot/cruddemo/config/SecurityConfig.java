504e5b7caacb17f100d912db8ae5590726df1234
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-10-01 11:11:12 +0300

    Secure endpoints

---
+++ REST API Security/src/main/java/com/luv2code/springboot/cruddemo/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.luv2code.springboot.cruddemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(config->
+                config
+                        .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(csrf->csrf.disable());
+        return http.build();
+    }
+
+
+
+
+    // @Bean
+//    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+//
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}test123")
+//                .roles("Employee")
+//                .build();
+//
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}test123")
+//                .roles("Employee","Manager")
+//                .build();
+//
+//
+//        UserDetails susan = User.builder()
+//                .username("susan")
+//                .password("{noop}test123")
+//                .roles("Employee","Manager","Admin")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(john, mary, susan);
+//    }
+}
